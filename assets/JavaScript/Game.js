e7ad367dfc83378af91940f08991ce3f915bea73
Gastonm1 <gaston.marques@gmail.com>
2018-12-07 19:13:03 -0600

    JS attempt
    12/7/2018

--- assets/JavaScript/Game.js
+++ assets/JavaScript/Game.js
@@ -1,6 +1,13 @@
 // Create an array of words
-var MarvelMovies = ['Iron Man' 'The Incredible Hulk' 'Thor' 'Captain America' 'Avengers' 'Guardians of the Galaxy' 'Ant-Man' 'Doctor Strange' 'Black Panther' 'Spider-Man'];
-// Choose word randomly
+var MarvelMovies = ['Iron Man', 'Incredible Hulk', 'Thor', 'Captain America', 'Avengers', 'Guardians of the Galaxy', 'Ant-Man', 'Doctor Strange', 'Black Panther', 'Spider-Man'];
+// Choose Movie randomly
+var ChoosenMovie = MarvelMovies[Math.floor(Math.random() * MarvelMovies.length)];
+function showMovies(){
+    document.getElementById(MarvelMovies).innerHTML = MarvelMovies[rand];
+}
+showMovies();
+
+console.log(ChoosenMovie);
 //Create underscores based on length of word
 //Obtain users guess
 // Check if guess is correct
