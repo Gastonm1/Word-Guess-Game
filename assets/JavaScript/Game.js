8d10f8cbb1546c6cc95fdd797ea81332c75b651a
Gastonm1 <gaston.marques@gmail.com>
2018-12-09 22:27:26 -0600

    Generated Underscores based on the random movie

--- assets/JavaScript/Game.js
+++ assets/JavaScript/Game.js
@@ -2,23 +2,29 @@
 var MarvelMovies = ['Iron Man', 'Incredible Hulk', 'Thor', 'Captain America', 'Avengers', 'Guardians of the Galaxy', 'Ant-Man', 'Doctor Strange', 'Black Panther', 'Spider-Man'];
 // Choose Movie randomly
 var randMovie = MarvelMovies[Math.floor(Math.random() * MarvelMovies.length)];
-// Selected 'Iron Man'
-//Create underscores based on length of word
+
+//Display chosen movie on page
 var generateUnderscore = () => {
     for (var i=0; i < randMovie.length; i++) {
     }   
-    console.log(document.querySelector(".Underscore").innerHTML = randMovie);
-
+    document.querySelector(".Underscore").innerHTML = randMovie;
 }
-console.log(generateUnderscore());
+generateUnderscore();
 
-//value of underscore
+//Create underscores based on length of word
+var underscore = "";
+    for (var i= 0; i< randMovie.length; i++) {
+        underscore = underscore + " _ ";
+    }
+    document.querySelector(".Underscore").innerHTML = underscore;
+console.log(underscore);
 
 //Obtain users guess
 document.addEventListener("keydown", function(event){
     console.log(event.key);
 });
 // Check if guess is correct
+if 
 // if right, push to underscores and correct array
 //If wrong, push to wrong array
 
@@ -27,3 +33,14 @@ document.addEventListener("keydown", function(event){
 ("_" + "_")  //concatinate string using for loop
 // once i have the string of underscores how do i add it the div?
 // once a user guesses how to add that to the string?
+
+var changeMe = 0;
+console.log(changeMe); // 0
+
+var iAmAFunction = () => {
+ changeMe = 2   
+}
+
+console.log(changeMe); // 0
+console.log(iAmAFunction()); // undefined
+console.log(changeMe); // 2
