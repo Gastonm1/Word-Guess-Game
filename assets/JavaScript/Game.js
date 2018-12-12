9d72d6878fc8c49daf444be5248ad3b877568922
Gastonm1 <gaston.marques@gmail.com>
2018-12-11 18:46:25 -0600

    Style & JS updates
    12.11.18

--- assets/JavaScript/Game.js
+++ assets/JavaScript/Game.js
@@ -1,3 +1,4 @@
+
 // Create an array of words
 var MarvelMovies = ['Iron Man', 'Incredible Hulk', 'Thor', 'Captain America', 'Avengers', 'Guardians of the Galaxy', 'Ant-Man', 'Doctor Strange', 'Black Panther', 'Spider-Man'];
 // Choose Movie randomly
@@ -22,25 +23,17 @@ console.log(underscore);
 //Obtain users guess
 document.addEventListener("keydown", function(event){
     console.log(event.key);
-});
-// Check if guess is correct
-if 
-// if right, push to underscores and correct array
-//If wrong, push to wrong array
+})
 
+if (randMovie.indexOf(randMovie['i']) > -1) 
+{
+    for (var i=0; i <randMovie.length; i++)
+    {
+         if(randMovie.charAt('i') === underscore)
 
-// how do i create a string of underscores?
-("_" + "_")  //concatinate string using for loop
-// once i have the string of underscores how do i add it the div?
-// once a user guesses how to add that to the string?
-
-var changeMe = 0;
-console.log(changeMe); // 0
-
-var iAmAFunction = () => {
- changeMe = 2   
+            underscore[i] = randMovie[i]
+    }
 }
 
-console.log(changeMe); // 0
-console.log(iAmAFunction()); // undefined
-console.log(changeMe); // 2
+// if userguess is right, display to underscore with letter
+// If userguess is wrong, remove one value from remaining guesses //
