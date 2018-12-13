b005c82fc22c35a55179d2d652034de25f9b69be
Gastonm1 <gaston.marques@gmail.com>
2018-12-12 23:32:30 -0600

    Losing alert

--- assets/JavaScript/Game.js
+++ assets/JavaScript/Game.js
@@ -8,7 +8,7 @@ document.getElementById("Wins").innerHTML= "You have won " + winCounter + " game
 var MarvelMovies = ['hulk', 'thor', 'avengers', 'antman', 'spiderman'];
 // Choose Movie randomly
 var randMovie = MarvelMovies[Math.floor(Math.random() * MarvelMovies.length)];
-
+console.log(randMovie);
 //Display chosen movie on page
 var generateUnderscore = () => {
     for (var i = 0; i < randMovie.length; i++) {
@@ -18,23 +18,27 @@ var generateUnderscore = () => {
 generateUnderscore();
 
 //Create underscores based on length of word
-var underscore = "";
+var underscore = [];
 for (var i = 0; i < randMovie.length; i++) {
     underscore = underscore + " _ ";
+
 }
+
 document.querySelector(".Underscore").innerHTML = underscore;
 console.log(underscore);
 
 function winLose() {
-    if (winCounter === randMovie.length) {
+    if (UserGuess === randMovie.length) {
         alert('You made Stan Lee proud!')
+
     }
     else if (GuessesLeft === 0) {
         alert('Stan Lee is not satisfied!');
     }
 }
+
 var UserGuess = '';
-var WrongLetter = '';
+var WrongLetter = [];
 
 //Obtain user guess
 document.addEventListener("keydown", function (event) {
@@ -48,6 +52,7 @@ document.addEventListener("keydown", function (event) {
             if (randMovie.charAt(i) === UserGuess) { // If the user guessed one of the letters
                 underscore[i] = UserGuess
                 console.log(underscore);
+
                 winCounter++;
                 winLose();
 
@@ -59,9 +64,14 @@ document.addEventListener("keydown", function (event) {
         console.log(GuessesLeft)
         winLose();
     }
+    document.querySelector('#RemainingGuesses').innerHTML = GuessesLeft
+    document.querySelector('.Underscore').innerHTML = underscore
+    document.querySelector('#Wins').innerHTML = winCounter
 })
+
 
 
 // Todo:
 // When the user guesses a correct letter, does the new 'underscore' value show on the page?
 // What happens when the user guesses a full movie title?
+// Win counter needs to only display when game is won,
